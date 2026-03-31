fe57447d757ed3a2a6965e56f048d832c3e5e780
naveenkumarj84 <naveenjanardha980@gmail.com>
2026-03-31 12:35:52 +0530

    Update websocket.go

--- backend/websocket.go
+++ backend/websocket.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var clients = make(map[*websocket.Conn]bool)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil)
+	clients[conn] = true
+}
+
+func broadcast(msg string) {
+	for client := range clients {
+		client.WriteMessage(websocket.TextMessage, []byte(msg))
+	}
+}
