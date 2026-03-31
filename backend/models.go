32ac98dab17e2463eb2136f6991d4a2fd777c760
naveenkumarj84 <naveenjanardha980@gmail.com>
2026-03-31 12:35:10 +0530

    Update models.go

--- backend/models.go
+++ backend/models.go
@@ -1,1 +1,20 @@
+package main
 
+type Geofence struct {
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Category    string        `json:"category"`
+	Coordinates [][]float64   `json:"coordinates"`
+}
+
+type Vehicle struct {
+	ID            string `json:"id"`
+	VehicleNumber string `json:"vehicle_number"`
+	DriverName    string `json:"driver_name"`
+}
+
+type Location struct {
+	VehicleID string  `json:"vehicle_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
